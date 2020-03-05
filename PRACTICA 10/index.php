f672288b1b7b87db751fb58628ed3939ad9c4fc4
julioRumaya <60304632+julioRumaya@users.noreply.github.com>
2020-03-04 23:33:54 -0600

    Add files via upload

---
+++ PRACTICA 10/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Documento</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<?php
+
+//comentario 1 linea
+echo"<h1>Hola mundo PHP</h1>";
+
+echo "<h1>"
+
+?>
+
+</body>
+</html>
+
